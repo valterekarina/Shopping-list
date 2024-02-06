7d1bc713e4ce7203ccd90dbdb5deba91c3ff423f
valterekarina <101064793+valterekarina@users.noreply.github.com>
2024-02-06 16:49:37 +0200

    protected routes

--- shopping-list-app/src/components/Home.js
+++ shopping-list-app/src/components/Home.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavBar } from "./NavBar";
+import { auth } from '../firebase';
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+          if (!user) {
+            navigate('/login');
+          }
+        });
+      }, []);
+
 return(
     <>
     <NavBar/>
     <h1>Home</h1>
     </>
-
 );
 }
 
--- shopping-list-app/src/components/Profile.js
+++ shopping-list-app/src/components/Profile.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { NavBar } from "./NavBar";
+import { useNavigate } from "react-router-dom";
+import { auth } from '../firebase';
 
 const Profile = () =>{
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+          if (!user) {
+            navigate('/login');
+          }
+        });
+      }, []);
+
     return(
         <>
             <NavBar/>
